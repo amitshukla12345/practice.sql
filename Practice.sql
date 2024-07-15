dc90e9e42d0ae8e173dd06773e45efb97792227e
amitshukla12345 <amitshukla22509@gmail.com>
2024-07-15 08:34:57 +0530

    first commit

---
+++ Practice.sql
@@ -0,0 +1,18 @@
+-- To create database
+CREATE DATABASE daffodils_school;
+
+-- TO see all databases
+SHOW DATABASES;
+
+-- To select database
+use daffodils_scool;
+-- To select database(use db_name)
+use daffodils_school;
+SHOW TABLES;
+use daffodils_school;
+Show TABLES;
+-- To delete database(DRP DATABASE Db_name)
+DROP DATABASE daffodils_school;
+
+
+SHOW DATABASES;
